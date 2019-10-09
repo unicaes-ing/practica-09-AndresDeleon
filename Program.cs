d2cc36e957284e0073130028a9f60c65c6dd2439
Andrés Enrique Lemus Deleón <53536791+AndresDeleon@users.noreply.github.com>
2019-10-09 11:58:55 -0600

    Práctica

---
+++ Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Práctica9
+{
+    class Program
+    {
+        public static bool rep = false;
+        public static List<string> listaFrutas = new List<string>();
+        static void Main(string[] args)
+        {
+            int op;
+            do
+            {
+                rep = false;
+                Console.Clear();
+                Console.WriteLine("MENÚ LISTA DE FRUTAS");
+                Console.WriteLine("===============================================");
+                Console.WriteLine("1- Agregar fruta a la lista");
+                Console.WriteLine("2- Mostrar lista de frutas");
+                Console.WriteLine("3- Insertar fruta en la lista");
+                Console.WriteLine("4- Eliminar fruta de la lista");
+                Console.WriteLine("5- Buscar fruta en la lista");
+                Console.WriteLine("6- Vaciar lista de frutas");
+                Console.WriteLine("7- Salir");
+                Console.WriteLine("===============================================");
+                Console.Write("Opción de desea realizar: ");
+                if (int.TryParse(Console.ReadLine(), out op) && op > 0 && op <= 7)
+                {
+                    Console.Clear();
+                    switch (op)
+                    {
+                        case 1:
+                            agregarFruta();
+                            break;
+                        case 2:
+                            mostrarLista();
+                            break;
+                        case 3:
+                            insertarFruta();
+                            break;
+                        case 4:
+                            eliminarFruta();
+                            break;
+                        case 5:
+                            buscarFruta();
+                            break;
+                        case 6:
+                            vaciarLista();
+                            break;
+                        case 7:
+                            salir();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                Console.ReadKey();
+            } while (rep == true || op != 7);
+        }
+
+        public static void agregarFruta()
+        {
+            Console.WriteLine("Fruta a agregar:");
+            string fruta = Console.ReadLine();
+            listaFrutas.Add(fruta);
+            Console.WriteLine("¡Fruta agregada con éxito!");
+        }
+
+        public static void mostrarLista()
+        {
+            Console.WriteLine("LISTADO DE FRUTAS");
+            Console.WriteLine("===============================================");
+            foreach (string fruta in listaFrutas)
+            {
+                int pos = listaFrutas.IndexOf(fruta);
+                Console.WriteLine((pos + 1) + "- " + fruta);
+            }
+        }
+
+        public static void insertarFruta()
+        {
+            do
+            {
+                Console.Clear();
+                rep = false;
+                Console.WriteLine("Posición en de la fruta que desea insertar:");
+                if (int.TryParse(Console.ReadLine(), out int pos) && pos > 0 && pos <= listaFrutas.Count())
+                {
+                    Console.WriteLine("Fruta a insertar en la posición " + pos);
+                    string fruta = Console.ReadLine();
+                    listaFrutas.Insert((pos - 1), fruta);
+                    Console.WriteLine("¡Fruta insertada con éxito!");
+                }
+                else
+                {
+                    Console.WriteLine("La posición tiene que estar entre los elementos de la lista y ser un entero positivo");
+                    Console.ReadKey();
+                }
+            } while (rep == true);
+        }
+
+        public static void eliminarFruta()
+        {
+            Console.WriteLine("Fruta que desea eliminar:");
+            string fruta = Console.ReadLine();
+            if (listaFrutas.Contains(fruta))
+            {
+                listaFrutas.Remove(fruta);
+                Console.WriteLine("¡Fruta eliminada con éxito!");
+            }
+            else
+            {
+                Console.WriteLine("No existe esta fruta en la lista");
+            }
+        }
+
+        public static void buscarFruta()
+        {
+            Console.WriteLine("Fruta que desea buscar:");
+            string fruta = Console.ReadLine();
+            if (listaFrutas.Contains(fruta))
+            {
+                Console.WriteLine("La fruta {0} si está en la lista", fruta);
+            }
+            else
+            {
+                Console.WriteLine("La fruta {0} no está en la lista", fruta);
+            }
+        }
+
+        public static void vaciarLista()
+        {
+            Console.WriteLine("Presione <enter> para vaciar toda la lista");
+            Console.ReadKey();
+            listaFrutas.RemoveRange(0, listaFrutas.Count());
+            Console.WriteLine("¡Lista vaciada con éxito!");
+        }
+
+        public static void salir()
+        {
+            Console.WriteLine("¡Gracias por usar nuestra lista de frutas!");
+            Console.WriteLine("**Presione cualquier tecla para salir**");
+        }
+    }
+}
---
+++ Práctica9_2.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Práctica9
+{
+    class Práctica9_2
+    {
+        //Andrés Lemus 02/10/2019
+        public struct Alumno
+        {
+            public string carnet;
+            public string nombre;
+            public string carrera;
+            private decimal cum;
+
+            public void setCum(decimal cum)
+            {
+
+                if (cum > 0 && cum <= 10)
+                {
+                    this.cum = cum;
+                }
+            }
+
+            public decimal getCum()
+            {
+                return cum;
+            }
+        }
+
+        public static Dictionary<string, Alumno> dicAlumno = new Dictionary<string, Alumno>();
+        public static bool rep = false;
+
+        static void Main(string[] args)
+        {
+            int op = 0;
+            do
+            {
+                Console.Clear();
+                rep = false;
+                Console.WriteLine("INFORMACIÓN DE ALUMNOS DE LA UNIVERSIDAD CATÓLICA DE EL SALVADOR");
+                Console.WriteLine("================================================================================");
+                Console.WriteLine("1- Agregar Alumno");
+                Console.WriteLine("2- Mostrar Alumnos");
+                Console.WriteLine("3- Eliminar Alumno");
+                Console.WriteLine("4- Buscar Alumno");
+                Console.WriteLine("5- Vaciar Diccionario");
+                Console.WriteLine("6- Salir");
+                Console.WriteLine("================================================================================");
+                Console.WriteLine("¿Qué opción desea realizar?");
+                if (int.TryParse(Console.ReadLine(), out op) && op > 0 && op <= 6)
+                {
+                    Console.Clear();
+                    rep = false;
+                    switch (op)
+                    {
+                        case 1:
+                            agregarAlumno();
+                            break;
+                        case 2:
+                            mostrarAlumno();
+                            break;
+                        case 3:
+                            eliminarAlumno();
+                            break;
+                        case 4:
+                            buscarAlumno();
+                            break;
+                        case 5:
+                            vaciarDic();
+                            break;
+                        case 6:
+                            salir();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Tiene que ser un entero entre 1 y 6 :(");
+                    Console.ReadKey();
+                    rep = true;
+                }
+            } while (rep == true || op != 6);
+            Console.ReadKey();
+        }
+
+        public static void agregarAlumno()
+        {
+            Alumno alumno = new Alumno();
+            do
+            {
+                Console.Clear();
+                rep = false;
+                //REVISAR ESTO PARA QUE NO OBTENGA CARNET REPETIDO
+                Console.Write("Carnet: ");
+                alumno.carnet = Console.ReadLine();
+                if (dicAlumno.ContainsKey(alumno.carnet))
+                {
+                    Console.WriteLine("Carnet no Valido, intentelo de nuevo :)");
+                    Console.ReadKey();
+                    rep = true;
+                }
+            } while (rep == true);
+            Console.Write("Nombre: ");
+            alumno.nombre = Console.ReadLine();
+            Console.Write("Carrera: ");
+            alumno.carrera = Console.ReadLine();
+            Console.Write("CUM: ");
+            alumno.setCum(Convert.ToDecimal(Console.ReadLine()));
+            dicAlumno.Add(alumno.carnet, alumno);
+            Console.WriteLine("¡Alumno agregado con exito!");
+            Console.ReadKey();
+        }
+
+        public static void mostrarAlumno()
+        {
+            Console.WriteLine("LISTADO DE ALUMNOS ALMACENADOS");
+            Console.WriteLine("================================================================================");
+            Console.WriteLine();
+            Console.WriteLine("{0,-12}    {1,-15}    {2,-32}    {3,-5}", "Carnet", "Nombre", "Carrera", "CUM");
+            Console.WriteLine("================================================================================");
+            foreach (KeyValuePair<string, Alumno> datos in dicAlumno)
+            {
+                Alumno alum = datos.Value;
+                Console.WriteLine("{0,-12}    {1,-15}    {2,-32}    {3:N1}", alum.carnet, alum.nombre, alum.carrera, alum.getCum());
+                Console.WriteLine();
+            }
+            Console.WriteLine("================================================================================");
+            Console.ReadKey();
+        }
+
+        public static void eliminarAlumno()
+        {
+            Console.Write("Carnet del alumno a eliminar: ");
+            string carnet = Console.ReadLine();
+            if (dicAlumno.ContainsKey(carnet))
+            {
+                dicAlumno.Remove(carnet);
+                Console.WriteLine("¡Alumno eliminado con exito!");
+            }
+            else
+            {
+                Console.WriteLine("Carnet no Valido, intentelo de nuevo :)");
+            }
+            Console.ReadKey();
+        }
+
+        public static void buscarAlumno()
+        {
+            Console.Write("Carnet del alumno a buscar: ");
+            string carnet = Console.ReadLine();
+            if (dicAlumno.ContainsKey(carnet))
+            {
+                List<Alumno> alm = new List<Alumno>();
+                Console.WriteLine("Nombre: " + dicAlumno[carnet].nombre);
+                Console.WriteLine("Carrera: " + dicAlumno[carnet].carrera);
+                Console.WriteLine("CUM: " + dicAlumno[carnet].getCum());
+            }
+            else
+            {
+                Console.WriteLine("Carnet no Valido, intentelo de nuevo :)");
+            }
+            Console.ReadKey();
+        }
+
+        public static void vaciarDic()
+        {
+            Console.WriteLine("Se vaciará el Diccionario, presione cualquier tecla para continuar");
+            Console.ReadKey();
+            dicAlumno.Clear();
+            Console.WriteLine("¡Diccionario vaciado con éxito!");
+            Console.ReadKey();
+        }
+
+        public static void salir()
+        {
+            Console.WriteLine("¡Gracias por usar nuestro diccionario!");
+            Console.WriteLine("**Presione cualquier tecla para salir**");
+        }
+    }
+}
